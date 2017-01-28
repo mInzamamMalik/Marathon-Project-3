892bb6b0b8207bc699da1b4305c9fce25bee5ac0
malikasinger1 <malikasinger1@gmail.com>
2017-01-28 14:30:33 +0500

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { InboxComponent } from './components/inbox/inbox.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    DashboardComponent,
+    InboxComponent
   ],
   imports: [
     BrowserModule,
